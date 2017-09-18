de12a90068bb4683d818ca52b0eb70757ca9af68
Julio Garcia <jg@innovation.rocks>
2017-09-18 18:39:57 +0200

    added animation to cursor

--- src/components/cursor.js
+++ src/components/cursor.js
@@ -11,7 +11,7 @@ export default props => {
     }
 
     const material = {
-        color: props.color,
+        color: '#ffffff',
         shader: 'flat',
         opacity: props.opacity || 0.9,
         transparent: true
@@ -24,12 +24,34 @@ export default props => {
 
     }
 
+    const animationFI ={
+        property: 'geometry.thetaLength',
+        dir: 'normal',
+        dur: 1000,
+        easing: 'easeInSine',
+        loop: false,
+        from: 0,
+        to: 360,
+        startEvents: 'fusing'
+    }
+
+    const animationFO = {
+        property: 'geometry.thetaLength',
+        dir: 'reverse',
+        dur: 100,
+        easing: 'easeInSine',
+        loop: false,
+        to: 0,
+        startEvents: 'click'
+    }
+
     return (
         <Entity geometry={geometry} material={material} position="0 0 -1">
-            <Entity cursor={{ fuse: true, fuseTimeout: 1000 }} geometry={circleGeometry} material={material} position="0 0 0"
+            <Entity cursor={{ fuse: true, fuseTimeout: 1000 }} geometry={circleGeometry} material={material}             position="0 0 0"
                 raycaster="objects: .clickable"
-                animation__scale="property: geometry.thetaStart; dir: noraml; dur: 1000; easing: easeInSine; loop:false; to: 360; startEvents:fusing"
-                animation__scaleoff="property: geometry.thetaStart; dir: reverse; dur: 150; easing: easeInSine; loop:false; to: 0; startEvents:click"
+                animation__scale = { animationFI }
+                animation__scaleoff={ animationFO }
+                event-set__1="_event:mouseleave; geometry.thetaLength: 0"
             >
             </Entity>
         </Entity>
